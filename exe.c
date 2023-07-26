0cfe01202092ca3e98cfcefc217f3825d977f386
abdallaaah <eng.abdallaeltawil@gmail.com>
2023-07-26 10:46:25 -0700

    check

--- exe.c
+++ exe.c
@@ -2,41 +2,37 @@
 void execute_me(char **tokens, char *dot, int num_tokens, char *buffer_line)
 {
     pid_t pid;
-    int ind;
-    int i = 0;
-    char *argv[2];
+    int ret;
     (void)num_tokens;
     (void)dot;
-    argv[0] = tokens[0];
-    argv[1] = NULL;
+
+    if (access(tokens[0], X_OK) == -1)
+    {
+        char error_message[BUFSIZ];
+        snprintf(error_message, BUFSIZ, "%s: %d: %s: not found\n", dot, 1, tokens[0]);
+        write(STDOUT_FILENO, error_message, strlen(error_message));
+        return;
+    }
+
     pid = fork();
     if (pid == -1)
     {
-        for (i = 0; i < num_tokens; i++)
-        {
-            free(tokens[i]);
-        }
-        free(tokens);
-        free(buffer_line);
         perror("fork error");
+        free_tokens(tokens, num_tokens);
+        free(buffer_line);
         exit(EXIT_FAILURE);
     }
     else if (pid == 0)
     {
-        ind = execve(argv[0], argv, NULL);
-        if (ind == -1)
+        ret = execve(tokens[0], tokens, NULL);
+        if (ret == -1)
         {
-            char error_message[BUFSIZ];
-            snprintf(error_message, BUFSIZ, "%s: %d: %s: not found\n", dot, 1, tokens[0]);
-            for (i = 0; i < num_tokens; i++)
-            {
-                free(tokens[i]);
-            }
-            free(tokens);
+            perror("execve error");
+            free_tokens(tokens, num_tokens);
             free(buffer_line);
-	    write(STDOUT_FILENO, error_message, strlen(error_message));
             exit(EXIT_FAILURE);
         }
+        
     }
     else
     {
